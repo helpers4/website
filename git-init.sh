ab23cbc028256ee397f6b95671febf1ba330121f
Bérenger <berenger@arnaud.xyz>
2022-09-05 23:04:32 +0200

    docs: :memo: fix doc namespaces

---
+++ git-init.sh
@@ -0,0 +1,6 @@
+# git submodule add https://github.com/helpers4/observable.git helpers4/observable/
+# git submodule add https://github.com/helpers4/promise.git helpers4/promise/
+# git submodule add https://github.com/helpers4/string.git helpers4/string/
+# git submodule add https://github.com/helpers4/url.git helpers4/url/
+
+git submodule update --init --recursive
