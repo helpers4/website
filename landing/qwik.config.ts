c18f21a938f931d70abf037a7ab426577c37edb1
baxyz <baxyz@etik.com>
2026-03-06 22:58:27 +0000

    feat(landing): :recycle: Reimplement WebAwesome usage

--- landing/qwik.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from '@builder.io/qwik/optimizer';
-
-export default defineConfig({
-  mdx: {
-    jsxImportSource: '@builder.io/qwik',
-  },
-});
---
+++ landing/src/main.ts
@@ -0,0 +1,4 @@
+import '@awesome.me/webawesome-pro/dist/styles/webawesome.css';
+import '@awesome.me/webawesome-pro/dist/styles/themes/tailspin.css';
+import '@awesome.me/webawesome-pro/dist/components/card/card.js';
+import '@awesome.me/webawesome-pro/dist/components/button/button.js';
